6ff21f4aa8270fe80054ee29b1008b5cb3ab5b37
Mariusz Dybala <mariusz.dybala@solidbrain.com>
2016-12-18 01:42:46 +0100

    Add list popup added

---
+++ HappyCoupleMobile/HappyCoupleMobile/HappyCoupleMobile/Mvvm/Controls/AddListPopUpView.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HappyCoupleMobile.Model;
+using Xamarin.Forms;
+
+namespace HappyCoupleMobile.Mvvm.Controls
+{
+    public partial class AddListPopUpView : Frame
+    {
+
+        public static readonly BindableProperty ListNameProperty = BindableProperty.Create(
+        nameof(ShoppingList), typeof(string), typeof(AddListPopUpView), propertyChanged: OnListNameChanged);
+
+        public string ListName
+        {
+            get { return (string)GetValue(ListNameProperty); }
+            set { SetValue(ListNameProperty, value); }
+        }
+
+        public AddListPopUpView()
+        {
+            InitializeComponent();
+        }
+
+        private static void OnListNameChanged(BindableObject bindable, object oldvalue, object newvalue)
+        {
+            if (newvalue == null || newvalue == oldvalue)
+            {
+                return;
+            }
+
+            var listName = (string)newvalue;
+            var addListPopUpView = (AddListPopUpView)bindable;
+        }
+    }
+}
