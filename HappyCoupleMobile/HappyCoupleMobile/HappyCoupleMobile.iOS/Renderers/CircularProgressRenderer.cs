61e662ee1105d96e70b2551091aaf95c8eb32fc6
Mariusz Dybala <mariusz.dybala@solidbrain.com>
2017-04-06 19:49:37 +0200

    Added curcural progress bar with list status

---
+++ HappyCoupleMobile/HappyCoupleMobile/HappyCoupleMobile.iOS/Renderers/CircularProgressRenderer.cs
@@ -0,0 +1,32 @@
+﻿
+using CoreGraphics;
+using DACircularProgress;
+using HappyCoupleMobile.iOS.Renderers;
+using HappyCoupleMobile.Mvvm.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(CircularProgress), typeof(CircularProgressRenderer))]
+namespace HappyCoupleMobile.iOS.Renderers
+{
+    public class CircularProgressRenderer : VisualElementRenderer<StackLayout>
+    {
+        protected override void OnElementChanged(ElementChangedEventArgs<StackLayout> e)
+        {
+            base.OnElementChanged(e);
+
+            base.OnElementChanged(e);
+
+            var rect = new CGRect(0,0, 50, 50);
+            var progressView = new CircularProgressView(rect);
+
+            progressView.RoundedCorners = true;
+            progressView.TrackTintColor = Color.FromHex("#F6585D").ToUIColor();
+            progressView.ProgressTintColor = Color.FromHex("#30AD63").ToUIColor();
+            progressView.SetProgress(0.5f, true);
+
+            AddSubview(progressView);
+        }
+    }
+}
